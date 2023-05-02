5f0dc3cd605cdbbd086e7441cedfc49b07f56a53
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-05-02 14:46:18 -0500

    change colors on text and borders and skills whit color about type

--- src/components/pokedex/PokemonCard.jsx
+++ src/components/pokedex/PokemonCard.jsx
@@ -4,13 +4,40 @@ import React, { useEffect, useState } from "react"
 
 const bordersByType = {
   grass: "border-green-600/40",
-  fire: "border-red-500",
+  fire: "border-orange-600/90",
+  water: "border-blue-500/90",
+  bug: "border-lime-700/70 to-lime-700/80",
+  normal: "border-normal",
+  
 }
 
 const backgroundByType = {
   grass: "bg-gradient-to-b from-cyan-400/70  to-lime-200/60",
-  fire: "border-red-500",
+  fire: "bg-gradient-to-b from-red-500 to-orange-400",
+  water: "bg-gradient-to-b from-blue-600 to-cyan-600",
+  bug: "bg-gradient-to-b from-lime-700/80 to-lime-500/70",
+  normal: "bg-gradient-to-b from-normal to-pink-950/70",
 }
+
+const nameColorByType = {
+  grass: "text-green-600",
+  fire: "text-orange-600",
+  bug: "text-green-700/80",
+  water: "text-blue-600",
+  electric: "text-yellow-600",
+  normal: " text-normal ",
+  
+}
+
+const SkillColorByType = {
+  grass: "text-green-600",
+  fire: "text-orange-600",
+  bug: "text-green-700/80",
+  water: "text-blue-600",
+  electric: "text-yellow-600",
+  normal: "text-normal",
+}
+
 
 const PokemonCard = ({pokemonUrl}) => {
   const [pokemon, setPokemon ] = useState()
@@ -27,7 +54,7 @@ const PokemonCard = ({pokemonUrl}) => {
   
 
   return (
-    <article className={`text-center border-8 rounded-xl ${bordersByType[pokemon?.types[0].type.name]}`}>
+    <article className={`text-center border-8 rounded-[14px] shadow-xl ${bordersByType[pokemon?.types[0].type.name]}`}>
 
       {/* Header */}
 
@@ -41,9 +68,12 @@ const PokemonCard = ({pokemonUrl}) => {
       {/* Footer */}
 
       <section>
-        <h3 className=" text-gray-600 mt-12 capitalize font-bold">{pokemon?.name}</h3>
-        <h4 className="capitalize font-bold text-sm">{types}</h4>
-        <span className="py-2 text-gray-400 text-xs  ">Type</span>
+      <h3 className={`mt-12 capitalize font-bold ${nameColorByType[pokemon?.types[0].type.name]}`}>
+        {pokemon?.name}
+        </h3>
+
+        <h4 className="capitalize font-semibold text-sm">{types}</h4>
+        <span className="py-2 text-gray-400 text-xs ">Type</span>
         
 
         <hr />
@@ -52,8 +82,8 @@ const PokemonCard = ({pokemonUrl}) => {
           {
             pokemon?.stats.map(stat => (
              <div key={stat.stat.name}>
-              <h5 className="uppercase text-gray-400 font-bold text-xs">{stat.stat.name}</h5>
-              <span className=" text-black/70 font-bold">{stat.base_stat}</span>
+              <h5 className="uppercase text-gray-400 font-semibold text-xs">{stat.stat.name}</h5>
+              <span className={`font-semibold ${SkillColorByType[pokemon?.types[0].type.name]}`}>{stat.base_stat}</span>
              </div>
             ))
           }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        normal: "rgba(115, 82, 89, 1)"
+      }
+    },
   },
   plugins: [],
 }
