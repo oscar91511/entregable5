d13d6cfa6425fbf509803882666327e287c6b17d
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-05-03 16:19:33 -0500

    finish whit stiles of pokemos cards and list

--- src/components/pokedex/PokemonCard.jsx
+++ src/components/pokedex/PokemonCard.jsx
@@ -1,7 +1,6 @@
-import axios from "axios"
-import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const bordersByType = {
   grass: "border-green-600/40",
@@ -9,16 +8,45 @@ const bordersByType = {
   water: "border-blue-500/90",
   bug: "border-lime-700/70 to-lime-700/80",
   normal: "border-normal",
-  
-}
+  fighting: "border-red-900/80",
+  flying: "border-teal-900/80 ",
+  poison: "border-purple-800/80 ",
+  ground: "border-yellow-900 ",
+  rock: "border-stone-600/90 ",
+  ghost: "border-blue-900/90",
+  steel: "border-blue-800/90",
+  electric: "border-yellow-500",
+  psychic: "border-yellow-600/80",
+  ice: "border-sky-400/90",
+  dragon: "border-teal-700/70",
+  dark: "border-black/90",
+  fairy: " border-red-400/80 ",
+  unknown: "",
+  shadow: "",
+};
 
 const backgroundByType = {
   grass: "bg-gradient-to-b from-cyan-400/70  to-lime-200/60",
   fire: "bg-gradient-to-b from-red-500 to-orange-400",
   water: "bg-gradient-to-b from-blue-600 to-cyan-600",
   bug: "bg-gradient-to-b from-lime-700/80 to-lime-500/70",
-  normal: "bg-gradient-to-b from-normal to-pink-950/70",
-}
+  normal: "bg-gradient-to-b from-normal to-pink-950/50",
+  fighting: "bg-gradient-to-b from-red-800 to-red-800/80",
+  flying: "bg-gradient-to-b from-teal-900/80 to-teal-900/60",
+  poison: "bg-gradient-to-b from-purple-600 to-purple-400",
+  ground: "bg-gradient-to-b from-yellow-900 to-yellow-800/90",
+  rock: "bg-gradient-to-b from-stone-500 to-stone-400",
+  ghost: "bg-gradient-to-b from-blue-800 to-indigo-900/90",
+  steel: "bg-gradient-to-b from-blue-800 to-blue-700/80",
+  electric: "bg-gradient-to-b from-yellow-400 to-amber-200",
+  psychic: "bg-gradient-to-b from-fuchsia-600/80 to-yellow-400/90",
+  ice: "bg-gradient-to-b from-sky-400/90 to-sky-300/80",
+  dragon: "bg-gradient-to-b from-teal-900/80 to-teal-900/60",
+  dark: "bg-gradient-to-b from-black to-black/80",
+  fairy: "bg-gradient-to-b from-red-300 to-red-200",
+  unknown: "",
+  shadow: "",
+};
 
 const nameColorByType = {
   grass: "text-green-600",
@@ -27,8 +55,22 @@ const nameColorByType = {
   water: "text-blue-600",
   electric: "text-yellow-600",
   normal: " text-normal ",
-  
-}
+  fighting: "text-red-800 ",
+  flying: " text-teal-900/80 ",
+  poison: "text-purple-600 ",
+  ground: "text-yellow-900 ",
+  rock: "text-stone-500 ",
+  ghost: "text-blue-800",
+  steel: "text-blue-800",
+  electric: "text-yellow-400 ",
+  psychic: "text-yellow-600/80",
+  ice: "text-sky-400 ",
+  dragon: "text-teal-900/80",
+  dark: "text-black/90",
+  fairy: "text-red-400/80",
+  unknown: "",
+  shadow: "",
+};
 
 const SkillColorByType = {
   grass: "text-green-600",
@@ -37,62 +79,96 @@ const SkillColorByType = {
   water: "text-blue-600",
   electric: "text-yellow-600",
   normal: "text-normal",
-}
+  fighting: "text-red-800",
+  flying: "text-teal-900/80",
+  poison: "text-purple-600 ",
+  ground: "text-yellow-900",
+  rock: "text-stone-500 ",
+  ghost: "text-blue-800",
+  steel: "text-blue-800 ",
+  electric: "text-yellow-400",
+  psychic: "text-yellow-600/80",
+  ice: "text-sky-400",
+  dragon: "text-teal-900/80",
+  dark: "text-black/90",
+  fairy: "text-red-400/80",
+  unknown: "",
+  shadow: "",
+};
 
+const PokemonCard = ({ pokemonUrl }) => {
+  const [pokemon, setPokemon] = useState();
 
-const PokemonCard = ({pokemonUrl}) => {
-  const [pokemon, setPokemon ] = useState()
-
-  const types = pokemon?.types.slice(0, 2).map(type => type.type.
-  name).join(" / ")
+  const types = pokemon?.types
+    .slice(0, 2)
+    .map((type) => type.type.name)
+    .join(" / ");
 
   useEffect(() => {
-    axios.get(pokemonUrl)
+    axios
+      .get(pokemonUrl)
       .then((res) => setPokemon(res.data))
-      .catch((err) => console.log(err))
-    
-  }, [])
-  
-  
-  return (
-    <Link to={`/pokedex/${pokemon?.id}`} className={`text-center border-8 rounded-[16px] drop-shadow-3xl bg-white ${bordersByType[pokemon?.types[0].type.name]}`}>
+      .catch((err) => console.log(err));
+  }, []);
 
+  return (
+    <Link
+      to={`/pokedex/${pokemon?.id}`}
+      className={`text-center border-8 rounded-[16px] drop-shadow-3xl bg-white ${
+        bordersByType[pokemon?.types[0].type.name]
+      }`}
+    >
       {/* Header */}
 
-      <section className={`rounded-t-[5px] relative h-[150px] ${backgroundByType[pokemon?.types[0].type.name] } `}>
+      <section
+        className={`rounded-t-[5px] relative h-[150px] ${
+          backgroundByType[pokemon?.types[0].type.name]
+        } `}
+      >
         <div className="absolute w-[200px] -bottom-15 left-1/2 -translate-x-1/2 first-letter">
-          <img className="pokemon-image hover:animate-waving-hand" src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
+          <img
+            className="pokemon-image hover:animate-waving-hand"
+            src={pokemon?.sprites.other["official-artwork"].front_default}
+            alt=""
+          />
         </div>
-
       </section>
 
       {/* Footer */}
 
       <section>
-      <h3 className={`mt-12 capitalize font-bold ${nameColorByType[pokemon?.types[0].type.name]}`}>
-        {pokemon?.name}
+        <h3
+          className={`mt-12 capitalize font-bold ${
+            nameColorByType[pokemon?.types[0].type.name]
+          }`}
+        >
+          {pokemon?.name}
         </h3>
 
         <h4 className="capitalize font-semibold text-sm">{types}</h4>
         <span className="py-2 text-gray-400 text-xs ">Type</span>
-        
 
         <hr />
-      
+
         <section className="grid grid-cols-3 gap-2 p-2  ">
-          {
-            pokemon?.stats.map(stat => (
-             <div key={stat.stat.name}>
-              <h5 className="uppercase text-gray-400 font-semibold text-xs ">{stat.stat.name}</h5>
-              <span className={`font-semibold  ${SkillColorByType[pokemon?.types[0].type.name]}`}>{stat.base_stat}</span>
-             </div>
-            ))
-          }
+          {pokemon?.stats.map((stat) => (
+            <div key={stat.stat.name}>
+              <h5 className="uppercase text-gray-400 font-semibold text-xs ">
+                {stat.stat.name}
+              </h5>
+              <span
+                className={`font-semibold  ${
+                  SkillColorByType[pokemon?.types[0].type.name]
+                }`}
+              >
+                {stat.base_stat}
+              </span>
+            </div>
+          ))}
         </section>
       </section>
-
     </Link>
-  )
-}
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
--- src/pages/PokemonId.jsx
+++ src/pages/PokemonId.jsx
@@ -27,7 +27,22 @@ const PokemonId = () => {
     fire: "bg-gradient-to-b from-red-500 to-orange-400",
     water: "bg-gradient-to-b from-blue-600 to-cyan-600",
     bug: "bg-gradient-to-b from-lime-700/80 to-lime-500/70",
-    normal: "bg-gradient-to-b from-normal to-pink-950/70",
+    normal: "bg-gradient-to-b from-normal to-pink-950/50",
+    fighting: "bg-gradient-to-b from-red-800 to-red-800/80",
+    flying: "bg-gradient-to-b from-teal-900/80 to-teal-900/60",
+    poison: "bg-gradient-to-b from-purple-800 to-purple-400",
+    ground: "bg-gradient-to-b from-yellow-900 to-yellow-800/90",
+    rock: "bg-gradient-to-b from-stone-500 to-stone-400",
+    ghost: "bg-gradient-to-b from-blue-800 to-indigo-900/9",
+    steel: "bg-gradient-to-b from-blue-800 to-blue-700/80",
+    electric: "bg-gradient-to-b from-yellow-400 to-amber-200",
+    psychic: "bg-gradient-to-b from-fuchsia-600/80 to-yellow-400/90",
+    ice: "bg-gradient-to-b from-sky-400/90 to-sky-300/80",
+    dragon: "bg-gradient-to-b from-teal-900/80 to-teal-900/60",
+    dark: "bg-gradient-to-b from-black to-black/80",
+    fairy: "bg-gradient-to-b from-red-300 to-red-200",
+    unknown: "",
+    shadow: "",
   };
 
   const nameColorByType = {
@@ -37,27 +52,55 @@ const PokemonId = () => {
     water: "text-blue-600",
     electric: "text-yellow-600",
     normal: " text-normal ",
-    
-  }
-  
+    fighting: "text-red-800 ",
+    flying: " text-teal-900/80 ",
+    poison: "text-purple-600 ",
+    ground: "text-yellow-900 ",
+    rock: "text-stone-500 ",
+    ghost: "text-blue-800",
+    steel: "text-blue-800",
+    electric: "text-yellow-400 ",
+    psychic: "text-yellow-600/80",
+    ice: "text-sky-400 ",
+    dragon: "text-teal-900/80",
+    dark: "text-black/90",
+    fairy: "text-red-400/80",
+    unknown: "",
+    shadow: "",
+  };
+
   const SkillColorByType = {
     grass: "text-green-600",
     fire: "text-orange-600",
     bug: "text-green-700/80",
     water: "text-blue-600",
     electric: "text-yellow-600",
-    normal: "text-normal",
-  }
+    normal: " text-normal ",
+    fighting: "text-red-800 ",
+    flying: " text-teal-900/80 ",
+    poison: "text-purple-600 ",
+    ground: "text-yellow-900 ",
+    rock: "text-stone-500 ",
+    ghost: "text-blue-800",
+    steel: "text-blue-800",
+    electric: "text-yellow-400 ",
+    psychic: "text-yellow-600/80",
+    ice: "text-sky-400 ",
+    dragon: "text-teal-900/80",
+    dark: "text-black/90",
+    fairy: "text-red-400/80",
+    unknown: "",
+    shadow: "",
+  };
 
   return (
     <section>
       <Header />
 
       <section className="px-2 py-14 bg-[url('/images/stadium.jpg')] bg-cover bg-no-repeat justify-center ">
-       
         <article className="max-w-[750px] mx-auto  drop-shadow-3xl p-4 pb-6 rounded-xl bg-white ">
           {/* section superior */}
-          
+
           <section
             className={`rounded-t-[5px] relative w-full h-[80px]  ${
               backgroundByType[pokemon?.types[0].type.name]
@@ -70,7 +113,8 @@ const PokemonId = () => {
                   pokemon?.sprites.versions["generation-v"]["black-white"]
                     .animated.front_default
                 }
-                alt="" rel="noopener noreferrer"
+                alt=""
+                rel="noopener noreferrer"
               />
             </div>
           </section>
@@ -84,60 +128,86 @@ const PokemonId = () => {
 
             <div className="grid grid-cols-[1fr_auto_1fr] items-center gap-2">
               <hr />
-              <h2 className={`capitalize font-bold ${nameColorByType[pokemon?.types[0].type.name]}`}>{pokemon?.name}</h2>
+              <h2
+                className={`capitalize font-bold ${
+                  nameColorByType[pokemon?.types[0].type.name]
+                }`}
+              >
+                {pokemon?.name}
+              </h2>
               <hr />
             </div>
 
-              <div className="flex justify-center gap-6 text-center">
+            <div className="flex justify-center gap-6 text-center">
+              <div>
                 <div>
-                  <div>
-                    <h5 className="text-sm">Weigth</h5>
-                    <span className={`font-semibold  ${SkillColorByType[pokemon?.types[0].type.name]}`}>{pokemon?.weight}</span>
-                  </div>
-                </div>
-
-                <div>
-                  <div>
-                    <h5 className="text-sm">Height</h5>
-                    <span className={`font-semibold  ${SkillColorByType[pokemon?.types[0].type.name]}`}>{pokemon?.height}</span>
-                  </div>
+                  <h5 className="text-sm">Weigth</h5>
+                  <span
+                    className={`font-semibold  ${
+                      SkillColorByType[pokemon?.types[0].type.name]
+                    }`}
+                  >
+                    {pokemon?.weight}
+                  </span>
                 </div>
               </div>
 
+              <div>
+                <div>
+                  <h5 className="text-sm">Height</h5>
+                  <span
+                    className={`font-semibold  ${
+                      SkillColorByType[pokemon?.types[0].type.name]
+                    }`}
+                  >
+                    {pokemon?.height}
+                  </span>
+                </div>
+              </div>
+            </div>
 
-              <section className="grid md:grid-cols-2 gap-4" >
-                  {/* types */}
+            <section className="grid md:grid-cols-2 gap-4">
+              {/* types */}
 
-                <section className="text-center">
+              <section className="text-center">
+                <h3 className="text-xl font-semibold">Types</h3>
 
-                  <h3 className="text-xl font-semibold">Types</h3>
-
-                  <section  className="grid grid-cols-2 gap-4 mt-4">
-                    {
-                      pokemon?.types.map( type =>  <article key={type.type.name} className={`p-2
+                <section className="grid grid-cols-2 gap-4 mt-4">
+                  {pokemon?.types.map((type) => (
+                    <article
+                      key={type.type.name}
+                      className={`p-2
                        px-8 border-[1px] text-black font-bold border-gray-300 text-center
-                        capitalize rounded-md ${backgroundByType[pokemon?.types[0].type.name]}`}>{type.type.name}</article>)
-                    }
-                  </section>
-
-                </section>
-
-                  {/* abilityes */}
-
-                <section className="text-center">
-
-                  <h3 className="text-xl font-semibold">Abilities</h3>
-
-                  <section  className="grid grid-cols-2 gap-4 mt-4">
-                    {
-                      pokemon?.abilities.map(ability => <article key={ability.ability.name}
-                      className={`p-2 text-black font-bold px-8 border-[1px] border-gray-300 text-center
-                      capitalize truncate rounded-md ${backgroundByType[pokemon?.types[0].type.name]} `} >{ability.ability.name}</article>)
-                    }
-                  </section>
+                        capitalize rounded-md ${
+                          backgroundByType[pokemon?.types[0].type.name]
+                        }`}
+                    >
+                      {type.type.name}
+                    </article>
+                  ))}
                 </section>
               </section>
 
+              {/* abilityes */}
+
+              <section className="text-center">
+                <h3 className="text-xl font-semibold">Abilities</h3>
+
+                <section className="grid grid-cols-2 gap-4 mt-4">
+                  {pokemon?.abilities.map((ability) => (
+                    <article
+                      key={ability.ability.name}
+                      className={`p-2 text-black font-bold px-8 border-[1px] border-gray-300 text-center
+                      capitalize truncate rounded-md ${
+                        backgroundByType[pokemon?.types[0].type.name]
+                      } `}
+                    >
+                      {ability.ability.name}
+                    </article>
+                  ))}
+                </section>
+              </section>
+            </section>
           </section>
 
           {/* section of stats */}
